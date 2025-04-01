1b9aeb6e39d21f48bbdb6cad0ce125778d6122a5
MartinTaroco <119903532+MartinTaroco@users.noreply.github.com>
2025-04-01 11:10:20 -0300

    Add files via upload

---
+++ Aritmetica.py
@@ -0,0 +1,14 @@
+#Desafio 1
+a = 4
+
+b = 5
+
+resultado_suma = a +b 
+print(resultado_suma)
+
+#Desafio 2
+area = a*b
+
+proceso =  f"Se calculará el área de un triángulo de ancho {a} y largo {b} cuya área será {area}"
+
+print (proceso)
