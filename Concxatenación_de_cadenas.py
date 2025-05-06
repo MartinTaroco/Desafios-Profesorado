962eca23afc26b267f0bdf764c33f5a53d3fc362
MartinTaroco <119903532+MartinTaroco@users.noreply.github.com>
2025-05-06 09:58:57 -0300

    Add files via upload

---
+++ Concxatenación_de_cadenas.py
@@ -0,0 +1,11 @@
+#Crea una función que reciba dos cadenas y devuelva una sola concatenada.
+
+
+def concatenar(texto1, texto2):
+  
+  return texto1 + texto2
+
+
+resultado = concatenar("Hola como estas", "Yo bien")
+
+print(resultado)
