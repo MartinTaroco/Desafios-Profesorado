4051c0ed2dd488034d476b3864ab0290a6b6c068
MartinTaroco <119903532+MartinTaroco@users.noreply.github.com>
2025-05-19 21:28:24 -0300

    Add files via upload

---
+++ suma_lista.py
@@ -0,0 +1,9 @@
+lista = [1, 2 , 10, 5600]  #Lissta cualquier
+
+suma = 0                  #contador desde 0
+
+for i in lista:                       
+  suma += i                   #Itero sobre cada elemento de la lista y los voy sumando al contador
+
+
+print(suma)
